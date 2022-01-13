ad0acc65517d1e6e676714d8aaa143f2e919af79
Laura Tozer <ljctozer@gmail.com>
2022-01-13 21:14:38 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { marked } from 'marked';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+marked.setOptions({
+  breaks: true
+})
+
+const renderer = new marked.Renderer()
+
+const placeholder = `# This is a heading
+## This is a sub-heading
+This is a [link](https://lauratozerart.com)
+> This is a block quote
+
+\`<p>Here's some inline code</p>\`
+
+\`\`\`
+//Multi-line code:
+function multiply() {
+  const array = [1, 2, 3, 4, 5]
+  return array.map(num => num * 2)
+}
+\`\`\`
+Here's a list:
+1. Sushi
+2. Pizza
+3. Quesadilla
+
+**Text in bold**
+
+![image](http://www.fillmurray.com/200/300)
+`
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      text: placeholder
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  render() {
+    return (
+        <div>
+          <div id="editor-div">
+            <h1>Markdown Previewer</h1>
+            <textarea id="editor" value={this.state.text} onChange={this.handleChange}></textarea>
+          </div>
+          <p id="preview" dangerouslySetInnerHTML={{
+            __html: marked(this.state.text, {renderer: renderer})
+          }}></p>
+        </div>
+      )
+  }
 }
 
 export default App;
